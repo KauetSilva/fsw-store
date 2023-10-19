88c6f7fe49442c7ef031973603d4e434ec2c39e2
ClecioKauet <kauetg4m3r@gmail.com>
2023-10-19 20:21:20 -0300

    feat: :sparkles: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -27,8 +27,18 @@ export default async function Home() {
             </div>
 
             <div className="mt-8">
+                <p className="font-semibold uppercase pl-5 mb-3">Ofertas</p>
                 <ProductList products={deals}/>
             </div>
+
+            <Image
+                src="/banner-home-02.png"
+                height={0}
+                width={0}
+                className="h-auto w-full px-5"
+                sizes="100vw"
+                alt="Até 55% de desconto em mouses!"
+            />
         </div>
     )
 }
