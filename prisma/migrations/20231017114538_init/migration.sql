93d9d03e0c46200202ccbd534626527d0b2e1e1d
ClecioKauet <kauetg4m3r@gmail.com>
2023-10-18 22:21:05 -0300

    feat: add seed script to prisma and imageUrls field to Product table

--- prisma/migrations/20231017114538_init/migration.sql
+++ prisma/migrations/20231017114538_init/migration.sql
@@ -22,8 +22,5 @@ CREATE TABLE "Product" (
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
-
 -- AddForeignKey
 ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
