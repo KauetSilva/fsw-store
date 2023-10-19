1ef9f49d89f01814a0ac25d696d965cd2dfda2e7
ClecioKauet <kauetg4m3r@gmail.com>
2023-10-18 21:03:49 -0300

    feat: :sparkles: add auth handler to header components

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,51 +1,104 @@
+"use client"
+
 import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarImage, AvatarFallback } from "./avatar";
+import { Separator } from "./separator";
+
 
 const Header = () => {
-    return <Card className="flex items-center justify-between p-[1.875rem]">
+    const { status, data } = useSession();
+    const handleLoginClick = async () => {
+        await signIn();
+    };
+
+    const handleLogoutClick = async () => {
+        await signOut();
+    };
+
+    return (
+      <Card className="flex items-center justify-between p-[1.875rem]">
         <Sheet>
-            <SheetTrigger asChild>
-                <Button size="icon" variant="outline">
-                    <MenuIcon />
-                </Button>
-            </SheetTrigger>
-        <SheetContent side="left">
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
             <SheetHeader className="text-left text-lg font-semibold">
-                Menu
+              Menu
             </SheetHeader>
 
-            <div className="mt-2 flex flex-col gap-2">
-            <Button variant="outline" className="w-full justify-start gap-2">
-                <LogInIcon size={16} />
-                Fazer Login
-            </Button>
-            <Button variant="outline" className="w-full justify-start gap-2">
+            {status === "authenticated" && data?.user && (
+                <div className="flex flex-col">
+                    <div className="flex items-center gap-2 py-4">
+                        <Avatar>
+                        <AvatarFallback>
+                            {data.user.name?.[0].toUpperCase()}
+                        </AvatarFallback>
+                            {data.user.image && (
+                                <AvatarImage src={data.user.image} alt="" />
+                            )}
+                        </Avatar>
+
+                        <div className="flex flex-col">
+                            <p className="font-medium">{data.user.name}</p>
+                            <p className="text-sm opacity-75">Boas compras!</p>
+                        </div>
+                    </div>
+                    <Separator />
+                </div>
+              )}
+
+            <div className="mt-4 flex flex-col gap-2">
+              {status === "unauthenticated" && (
+                <Button
+                  onClick={handleLoginClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogInIcon size={16} />
+                  Fazer Login
+                </Button>
+              )}
+              {status === "authenticated" && (
+                <Button
+                  onClick={handleLogoutClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogInIcon size={16} />
+                  Fazer Logout
+                </Button>
+              )}
+              <Button variant="outline" className="w-full justify-start gap-2">
                 <HomeIcon size={16} />
                 Inicio
-            </Button>
-            <Button variant="outline" className="w-full justify-start gap-2">
+              </Button>
+              <Button variant="outline" className="w-full justify-start gap-2">
                 <PercentIcon size={16} />
                 Ofertas
-            </Button>
-            <Button variant="outline" className="w-full justify-start gap-2">
+              </Button>
+              <Button variant="outline" className="w-full justify-start gap-2">
                 <ListOrderedIcon size={16} />
                 Catálogo
-            </Button>
+              </Button>
             </div>
-        </SheetContent>
+          </SheetContent>
         </Sheet>
 
-
         <h1 className="text-lg font-semibold">
-            <span className="text-primary">FSW</span>Store
+          <span className="text-primary">FSW</span>Store
         </h1>
 
         <Button size="icon" variant="outline">
-            <ShoppingCartIcon />
+          <ShoppingCartIcon />
         </Button>
-    </Card>;
+      </Card>
+    );
 }
  
 export default Header;
