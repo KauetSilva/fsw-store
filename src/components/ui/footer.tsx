35ee6956fa3ac9bd3527f93de2b9a59086e8e532
ClecioKauet <kauetg4m3r@gmail.com>
2023-10-22 19:06:13 -0300

    feat: :sparkles: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+ const footer = () => {
+    return (
+        <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+            2023 Copyright <span className="font-semibold">FWS Store</span>
+        </div> 
+    );
+ }
+  
+ export default footer;
