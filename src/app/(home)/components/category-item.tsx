6301243aec9ab39aa21006749c398706aabc0248
ClecioKauet <kauetg4m3r@gmail.com>
2023-10-24 00:33:32 -0300

    feat: :sparkles: ensure user goes to category page on category item

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,26 +1,24 @@
 import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
-import { HeadphonesIcon, KeyboardIcon, MonitorIcon, MouseIcon, SpeakerIcon, SquareIcon } from "lucide-react";
+import Link from "next/link";
 
 interface CategoryItemProps {
-    category: Category
+  category: Category;
 }
 
-const CategoryItem = ({category}: CategoryItemProps) => {
-    const categoryIcon = {
-        keyboards: <KeyboardIcon size={16}/>,
-        monitors: <MonitorIcon size={16}/>,
-        headphones: <HeadphonesIcon size={16}/>,
-        mousepads: <SquareIcon size={16}/>,
-        speakers: <SpeakerIcon size={16}/>,
-        mouses: <MouseIcon size={16}/>,
-    }
-    return ( 
-        <Badge variant="outline" className="py-3 flex justify-center items-center gap-2 rounded-lg">
-            {categoryIcon[category.slug as keyof typeof categoryIcon]}
-            <span className="text-xs font-bold">{category.name}</span>
-        </Badge>
-     );
-}
- 
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  return (
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
+  );
+};
+
 export default CategoryItem;
